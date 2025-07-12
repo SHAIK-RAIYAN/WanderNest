fd3230be4e5ff199c524c412a143375f36f3cf64
SHAIK RAIYAN <shaikraiyan2005@gmail.com>
2025-07-12 15:32:09 +0530

    created express-session and flash msgs

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const methodOverride = require("method-override");
-const ejsMate = require("ejs-mate");
+const methodOverride = require("method-override"); //for patch and delete req
+const ejsMate = require("ejs-mate"); //to include other ejs files in a ejs file
 const ExpressError = require("./utils/ExpressError"); // error handling
+const session = require("express-session"); //for establishing a session
+const flash = require("connect-flash"); //to show a flash msg when work (like post) is done
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -15,6 +17,25 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
 app.engine("ejs", ejsMate);
+
+//express session
+const sessionOptions = {
+  secret: "secretCode",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000, //from current time to 7 days
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true, //to avoid cross-scripting attacks
+  },
+};
+app.use(session(sessionOptions));
+app.use(flash());
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
+});
 
 //routes
 const listings = require("./routes/listing.js");
